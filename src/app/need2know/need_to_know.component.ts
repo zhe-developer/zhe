ace63a27fe0625c5d77cd8b90f4983d43fc6573e
angelamatlapeng <tumiematlaps@gmail.com>
2017-01-20 12:48:05 +0200

    content update
    Added .css and .html files to project

--- src/app/need2know/need_to_know.component.ts
+++ src/app/need2know/need_to_know.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-need2know',
-  templateUrl: './need2know.component.html',
-  styleUrls: ['./need2know.component.css']
+  templateUrl: './need_to_know.component.html',
+  styleUrls: ['./need_to_know.component.css']
 })
 export class Need2knowComponent implements OnInit {
 
--- src/app/quoterequest/request_quote.component.ts
+++ src/app/quoterequest/request_quote.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quoterequest',
-  templateUrl: './quoterequest.component.html',
-  styleUrls: ['./quoterequest.component.css']
+  templateUrl: './request_quote.component.html',
+  styleUrls: ['./request_quote.component.css']
 })
 export class QuoterequestComponent implements OnInit {
 
